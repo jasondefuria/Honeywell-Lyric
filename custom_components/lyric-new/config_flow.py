156e374cb2e013ee879d714c6454d43d86ff8ecf
jasondefuria <41909811+jasondefuria@users.noreply.github.com>
2020-08-02 14:39:24 -0400

    Update config_flow.py

--- custom_components/lyric-new/config_flow.py
+++ custom_components/lyric-new/config_flow.py
@@ -56,7 +56,7 @@ class LyricFlowHandler(ConfigFlow):
                 client_id = self.client_id
                 client_secret = self.client_secret
                 redirect_uri = '{}{}'.format(
-                    self.get_url(hass), AUTH_CALLBACK_PATH)
+                    self.hass.helpers.network.get_url, AUTH_CALLBACK_PATH)
                 token_cache_file = self.hass.config.path(
                     CONF_LYRIC_CONFIG_FILE)
 
